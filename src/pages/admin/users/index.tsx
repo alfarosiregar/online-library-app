a738a64b88b6a508fc48f2d84a43679637a5e61b
Afdol Fahmul Rozi <afdolfarosir23@gmail.com>
2025-02-20 18:14:28 +0700

    Update : Authentication and authorization config

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,12 +1,18 @@
 import AdminUsersView from "@/components/views/admin/users";
 import userServices from "@/services/user";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 const AdminUsersPage = () => {
   const [users, setUsers] = useState([]);
+  const { data: session } = useSession(); // Ambil sesi autentikasi
+
+  const accessToken =
+    (session as any)?.accessToken || (session as any)?.user?.accessToken;
+
   useEffect(() => {
     const getAllUsers = async () => {
-      const { data } = await userServices.getAllUsers();
+      const { data } = await userServices.getAllUsers(accessToken);
       setUsers(data.data);
     };
     getAllUsers();
